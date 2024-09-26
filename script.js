15db9ed124116724696ebce6ace41635b80a17af
chetechy <89038400+chetechy@users.noreply.github.com>
2024-09-26 15:01:11 -0400

    added playRound fuction

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function getComputerChoice(){
         return 'Paper';
     } else {
         return 'Scissors'
-    }
-    
+    }  
 }
 
 function getHumanChoice(){
     let huChoice = prompt('Choose P for Paper, S for Scissors, or R for Rock').toLowerCase();
+    console.log(huChoice);
     if(huChoice === 'p'){
         return 'Paper';
     }else if(huChoice === 'r'){
@@ -24,3 +24,29 @@ function getHumanChoice(){
         return getHumanChoice();
     }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(huChoice, pcChoice){
+
+    if (huChoice === pcChoice){
+        console.log('Draw!')
+    } else if(
+        (huChoice === 'Rock' && pcChoice === 'Scissors') || 
+        (huChoice === 'Paper' && pcChoice === 'Rock')|| 
+        (huChoice === 'Scissors' && pcChoice === 'Paper') 
+    ){
+        console.log(huChoice + ' beats ' + pcChoice + ', You won this round!' );
+        humanScore++;
+    }else{
+        console.log('You lost this round! ' + pcChoice + ' beats ' + huChoice + '.');
+        computerScore++;
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
