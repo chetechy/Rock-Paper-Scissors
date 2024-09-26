55e839299a918c8102d78dffa4e3a7351cfd7977
chetechy <89038400+chetechy@users.noreply.github.com>
2024-09-26 15:40:46 -0400

    added playGame function, added scores variable to playRound function statement, cleaned up code.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let huChoice = prompt('Choose P for Paper, S for Scissors, or R for Rock').toLowerCase();
-    console.log(huChoice);
+
     if(huChoice === 'p'){
         return 'Paper';
     }else if(huChoice === 'r'){
@@ -25,8 +25,6 @@ function getHumanChoice(){
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
 function playRound(huChoice, pcChoice){
 
@@ -44,9 +42,48 @@ function playRound(huChoice, pcChoice){
         computerScore++;
     }
 
+    scores = 'Player score: ' + humanScore + '  ' + 'Computer score: ' + computerScore;
+    console.log(scores);
+
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
 
-playRound(humanSelection, computerSelection);
+function playGame(){
+
+    console.log('ROUND 1')
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+
+    console.log('ROUND 2')
+    const humanSelection2 = getHumanChoice();
+    const computerSelection2 = getComputerChoice();
+    playRound(humanSelection2, computerSelection2);
+
+    console.log('ROUND 3')
+    const humanSelection3 = getHumanChoice();
+    const computerSelection3 = getComputerChoice();
+    playRound(humanSelection3, computerSelection3);
+
+    console.log('ROUND 4')
+    const humanSelection4 = getHumanChoice();
+    const computerSelection4 = getComputerChoice();
+    playRound(humanSelection4, computerSelection4);
+
+    console.log('ROUND 5')
+    const humanSelection5 = getHumanChoice();
+    const computerSelection5 = getComputerChoice();
+    playRound(humanSelection5, computerSelection5);
+
+    if (humanScore > computerScore){
+        console.log('\nCongratulations, You Win! \n\nFinal Scores ' + '\n'+scores);
+    }else if(computerScore > humanScore){
+        console.log('\nSorry, You Lose! Better luck next time.\n\nFinal Scores ' + '\n'+scores);
+    }else{
+        console.log('\nThis game is Draw! \n\nFinal Scores ' + '\n'+scores);
+    }
+}
+
+playGame();
